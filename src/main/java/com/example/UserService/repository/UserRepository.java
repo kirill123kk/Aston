f03d1fe6bd9baf808722fb404a5b66771f0cf5a7
kirill123kk <89303490488@bk.ru>
2025-08-29 16:22:00 +0300

    5 Homework

--- src/main/java/com/example/UserService/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserService.repository;
-
-import com.example.UserService.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity,Long> {
-}
--- src/main/resources/db/changelog/0.0.1/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users
-(
-    id         BIGSERIAL PRIMARY KEY,
-    name       VARCHAR(255)        NOT NULL,
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    age        INT                         ,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
